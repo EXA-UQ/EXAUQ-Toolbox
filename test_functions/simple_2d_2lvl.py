2dcc56ef6aab65d717c07d35d620a38e89c536e7
mattj <m.b.johns@exeter.ac.uk>
2022-04-01 12:01:13 +0100

    Added Simple 2D, 2 Level Simulator PH Classes

---
+++ test_functions/simple_2d_2lvl.py
@@ -0,0 +1,51 @@
+from exauq.simulator import Simulator, SimStatus
+
+"""
+A Simple 2D, 2 Level Toy Problem as defined in... 
+"""
+
+
+class Simple2DLvl0(Simulator):
+    """
+    Simple 2D Test Function
+    """
+    def run(self) -> None:
+        """
+        Method to run the simulator
+        """
+        pass
+
+    def sim_status(self) -> SimStatus:
+        """
+        Method to check current status of simulation
+        """
+        pass
+
+    def write_to_database(self) -> None:
+        """
+        Method to write simulation data to database
+        """
+        pass
+
+
+class Simple2DLvl1(Simulator):
+    """
+    Simple 2D Test Function
+    """
+    def run(self) -> None:
+        """
+        Method to run the simulator
+        """
+        pass
+
+    def sim_status(self) -> SimStatus:
+        """
+        Method to check current status of simulation
+        """
+        pass
+
+    def write_to_database(self) -> None:
+        """
+        Method to write simulation data to database
+        """
+        pass
