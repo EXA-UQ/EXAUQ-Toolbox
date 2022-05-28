f1ca981beca28369ebbd42b63b4c521e5fd5fac0
Enrico Olivier <eao216@FVFH122KQ0F5.local>
2022-05-28 07:52:39 +0100

    quick minor refactor to reduce code duplication

--- utilities/secure_shell.py
+++ utilities/secure_shell.py
@@ -1,6 +1,35 @@
 import subprocess
 import typing
 
+def ssh_run_command(host_machine: str, username: str, command: str) -> typing.Tuple[str]:
+    """
+    Run a command on host via ssh.
+
+    Parameters
+    ----------
+    host_machine: str
+        host machine name
+    username: str
+        username to run job on remote machine
+    command: str
+        command to run on host machine
+
+    Returns
+    -------
+    Tuple:
+        A tuple of two strings, the stdout and stderr in that order
+    """
+    ssh_command = ['ssh', username + '@' + host_machine, command]
+    process = subprocess.Popen(ssh_command,
+                     stdout = subprocess.PIPE, 
+                     stderr = subprocess.PIPE,
+                     text = True,
+                     shell = False
+                     )
+    std_out, std_err = process.communicate()
+    return std_out, std_err
+    
+
 def ssh_submit_job(host_machine: str, username: str, command: str) -> str:
     """
     Submit a job to a host via ssh. All jobs are submitted using the at scheduler.
@@ -20,18 +49,12 @@ def ssh_submit_job(host_machine: str, username: str, command: str) -> str:
         The id of submitted job on host machine
     """
     job_id = None
-
     run_delay = '2 minutes'
     submit_command = 'echo ' + '"' + command + '" | at now + ' + run_delay
-    ssh_command = ['ssh', username + '@' + host_machine, submit_command]
-    process = subprocess.Popen(ssh_command,
-                     stdout = subprocess.PIPE, 
-                     stderr = subprocess.PIPE,
-                     text = True,
-                     shell = False
-                     )
-    std_err, std_out = process.communicate() # There is something seriously odd going on here
-                                             # the output from stdout seems to go into stderr.
+    # The redirection below is because at appears to send job details to stderr
+    # when started 
+    submit_command = submit_command + ' 2>&1'
+    std_out, std_err = ssh_run_command(host_machine=host_machine, username=username, command=submit_command) 
     if std_err:
         print('ssh job submission failed with: ' + std_err)
         job_id = None
@@ -61,14 +84,7 @@ def ssh_check_job_status(host_machine: str, username: str, job_id: str) -> str:
     """
     status = None
     check_status_command = 'atq'
-    ssh_command = ['ssh', username + '@' + host_machine, check_status_command]
-    process = subprocess.Popen(ssh_command,
-                     stdout = subprocess.PIPE, 
-                     stderr = subprocess.PIPE,
-                     text = True,
-                     shell = False
-                     )
-    std_out, std_err = process.communicate()
+    std_out, std_err = ssh_run_command(host_machine=host_machine, username=username, command=check_status_command)
     if std_err:
         print('ssh job status check failed with: ' + std_err)
         status = None
