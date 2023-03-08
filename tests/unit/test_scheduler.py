2fd9d52b1f0112f2794293da83804563b5cae8c8
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-03-08 17:51:06 +0000

    Convert unit tests to use unittest

--- tests/unit/test_scheduler.py
+++ tests/unit/test_scheduler.py
@@ -1,4 +1,4 @@
-import pytest
+from unittest import TestCase, main
 import time
 
 from exauq.core.scheduler import Scheduler
@@ -13,31 +13,36 @@ from test_functions.dummy_sims import (
 )
 
 
-def test_scheduler() -> None:
-    """
-    This tests the scheduler by running the four dummy simulations. The test
-    passes if all the submitted jobs has completed successfully. Send shutdown
-    signal to the scheduler once the requested job queue is cleared.
-    """
-    sim_factory = SimulatorFactory(
-        {
-            "lvl0": DummySimLvl0,
-            "lvl1": DummySimLvl1,
-            "lvl2": DummySimLvl2,
-            "lvl3": DummySimLvl3,
-        }
-    )
-    scheduler = Scheduler(
-        simulator_factory=sim_factory, scheduler_period=1, polling_period=2
-    )
-    scheduler.start_up()
-    list_of_jobs = [({}, "lvl0"), ({}, "lvl1"), ({}, "lvl2"), ({}, "lvl3")]
-    for job in list_of_jobs:
-        scheduler.request_job(parameters=job[0], sim_type=job[1])
-        time.sleep(10)
-    scheduler.shutdown()
-    success = all(
-        sim.JOBHANDLER.job_status == JobStatus.SUCCESS
-        for sim in scheduler.requested_job_list
-    )
-    assert success is True
+class TestScheduler(TestCase):
+    def test_scheduler(self) -> None:
+        """
+        This tests the scheduler by running the four dummy simulations. The test
+        passes if all the submitted jobs has completed successfully. Send shutdown
+        signal to the scheduler once the requested job queue is cleared.
+        """
+        sim_factory = SimulatorFactory(
+            {
+                "lvl0": DummySimLvl0,
+                "lvl1": DummySimLvl1,
+                "lvl2": DummySimLvl2,
+                "lvl3": DummySimLvl3,
+            }
+        )
+        scheduler = Scheduler(
+            simulator_factory=sim_factory, scheduler_period=1, polling_period=2
+        )
+        scheduler.start_up()
+        list_of_jobs = [({}, "lvl0"), ({}, "lvl1"), ({}, "lvl2"), ({}, "lvl3")]
+        for job in list_of_jobs:
+            scheduler.request_job(parameters=job[0], sim_type=job[1])
+            time.sleep(10)
+        scheduler.shutdown()
+        success = all(
+            sim.JOBHANDLER.job_status == JobStatus.SUCCESS
+            for sim in scheduler.requested_job_list
+        )
+        self.assertTrue(success)
+
+
+if __name__ == "__main__":
+    main()
