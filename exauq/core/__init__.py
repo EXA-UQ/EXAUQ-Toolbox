f531b466bda23b8783557741701cce1c37c1f0b5
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-05-04 16:41:17 +0100

    Revert "Issue 37: single level adaptive sampling initial design (#51)"
    This reverts commit 8d9805553e7b4dc362cc5899733a1d5b4feed55a.

--- exauq/core/__init__.py
+++ exauq/core/__init__.py
@@ -1,5 +1,7 @@
+from exauq.core.designer import Designer
 from exauq.core.event import EventType
 from exauq.core.event import Event
 from exauq.core.scheduler import Scheduler
 from exauq.core.simulator import Simulator
 from exauq.core.simulator import SimulatorFactory
+from exauq.core.surrogate import Surrogate
