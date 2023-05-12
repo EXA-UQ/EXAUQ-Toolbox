25cf4a9664ee292854ccb8f91fdd152af4b2a886
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-05-12 17:14:32 +0100

    Add documentation for validation functions
    Including type hinting.

--- exauq/utilities/validation/real.py
+++ exauq/utilities/validation/real.py
@@ -3,15 +3,25 @@
 
 from collections.abc import Iterable
 from numbers import Real
-from typing import Any
+from typing import (
+    Any,
+    Callable
+    )
 import numpy as np
 
 
-def check_entries_not_none(x: Iterable, exception: Exception):
+def check_entries_not_none(x: Iterable, exception: Exception) -> None:
+    """Raise the given exception if one of the elements of an iterable is
+    ``None``.
+    """
     check_for_each(check_not_none, x, exception)
 
 
-def check_for_each(check_function, x, exception):
+def check_for_each(
+        check_function: Callable, x: Iterable, exception: Exception
+        ) -> None:
+    """Apply a checking function to each element of an iterable, raising the
+    given exception if the check fails for some element."""
     try:
         for element in x:
             check_function(element, Exception())
@@ -20,24 +30,31 @@ def check_for_each(check_function, x, exception):
         raise exception
 
 
-def check_not_none(x: Any, exception: Exception):
+def check_not_none(x: Any, exception: Exception) -> None:
+    """Raise a given exception if an object is ``None``."""
     if x is None:
         raise exception
 
 
-def check_entries_real(x: Iterable, exception: Exception):
+def check_entries_real(x: Iterable, exception: Exception) -> None:
+    """Raise the given exception if one of the elements of an iterable is not
+    a real number."""
     check_for_each(check_real, x, exception)
 
 
-def check_real(x: Any, exception: Exception):
+def check_real(x: Any, exception: Exception) -> None:
+    """Raise the given exception if an object is not a real number."""
     if not isinstance(x, Real):
         raise exception
 
 
-def check_entries_finite(x: Iterable, exception: Exception):
+def check_entries_finite(x: Iterable, exception: Exception) -> None:
+    """Raise the given exception if one of the elements of an iterable is not
+    a finite number."""
     check_for_each(check_finite, x, exception)
 
 
-def check_finite(x: Any, exception: Exception):
+def check_finite(x: Any, exception: Exception) -> None:
+    """Raise the given exception if an object is not a finite number."""
     if not np.isfinite(x):
         raise exception
