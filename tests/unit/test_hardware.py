7fe81348f7091b1de7360cd44c204dcd22a64dbf
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2023-06-26 11:41:56 +0100

    black `test_hardware.py`

--- tests/unit/test_hardware.py
+++ tests/unit/test_hardware.py
@@ -12,17 +12,20 @@ class TestHardwareInterface(unittest.TestCase):
     # Test that an error is raised when trying to instantiate the abstract base class
     def test_abc_instantiation(self):
         with self.assertRaises(TypeError):
-            a = HardwareInterface(hostname="local",
-                                  username="DiscoPotato",
-                                  password="GroovySpuds777!")
+            a = HardwareInterface(
+                hostname="local", username="DiscoPotato", password="GroovySpuds777!"
+            )
 
-    # Test that an error is raised when trying to instantiate a subclass that doesn't implement all abstract methods
+    # Test that an error is raised when trying to instantiate a subclass that doesn't implement
+    # all abstract methods
     def test_dummy_subclass_instantiation(self):
         with self.assertRaises(TypeError):
-            b = DummyHardwareInterface(hostname="local",
-                                       username="SpudtasticWizard",
-                                       password="MagicalPotatoes42#")
+            b = DummyHardwareInterface(
+                hostname="local",
+                username="SpudtasticWizard",
+                password="MagicalPotatoes42#",
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
