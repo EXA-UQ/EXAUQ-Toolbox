d7fea79655fa23e44d1e463b40dc90e632f2f269
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2023-07-05 16:44:07 +0100

    remove `test_dummy_subclass_instantiation` from hardware tests

--- tests/unit/test_hardware.py
+++ tests/unit/test_hardware.py
@@ -3,28 +3,11 @@ import unittest
 from exauq.core.hardware import HardwareInterface
 
 
-# Define a dummy subclass that does not implement the abstract method
-class DummyHardwareInterface(HardwareInterface):
-    pass
-
-
 class TestHardwareInterface(unittest.TestCase):
     # Test that an error is raised when trying to instantiate the abstract base class
     def test_abc_instantiation(self):
         with self.assertRaises(TypeError):
-            a = HardwareInterface(
-                hostname="local", username="DiscoPotato", password="GroovySpuds777!"
-            )
-
-    # Test that an error is raised when trying to instantiate a subclass that doesn't implement
-    # all abstract methods
-    def test_dummy_subclass_instantiation(self):
-        with self.assertRaises(TypeError):
-            b = DummyHardwareInterface(
-                hostname="local",
-                username="SpudtasticWizard",
-                password="MagicalPotatoes42#",
-            )
+            a = HardwareInterface()
 
 
 if __name__ == "__main__":
