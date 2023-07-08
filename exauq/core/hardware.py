3b81c7ab1d59710020720c63b831fb94eba3daea
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2023-07-08 15:21:15 +0100

    added `SSHInterface` class

--- exauq/core/hardware.py
+++ exauq/core/hardware.py
@@ -22,3 +22,28 @@ class HardwareInterface(ABC):
     @abstractmethod
     def wait_for_job(self, job_id):
         pass
+
+
+class SSHInterface(HardwareInterface):
+    def __init__(self, user, host, password):
+        pass
+
+    @abstractmethod
+    def submit_job(self, job):
+        pass
+
+    @abstractmethod
+    def get_job_status(self, job_id):
+        pass
+
+    @abstractmethod
+    def get_job_output(self, job_id):
+        pass
+
+    @abstractmethod
+    def cancel_job(self, job_id):
+        pass
+
+    @abstractmethod
+    def wait_for_job(self, job_id):
+        pass
