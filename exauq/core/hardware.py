7f38ba28cea5e0719b36fcaeba97406ac9be5beb
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-07-24 14:13:07 +0100

    Format code as monospaced in docstring

--- exauq/core/hardware.py
+++ exauq/core/hardware.py
@@ -52,9 +52,9 @@ class SSHInterface(HardwareInterface):
     ssh_config_path : str, optional
         The path to the SSH configuration file.
     use_ssh_agent : bool, optional
-        If True, use SSH agent for authentication. Defaults to False.
+        If ``True``, use SSH agent for authentication. Defaults to ``False``.
     max_attempts : int, optional
-        The number of authentication attempts allowed. Defaults to 3.
+        The number of authentication attempts allowed. Defaults to ``3``.
 
     Raises
     ------
