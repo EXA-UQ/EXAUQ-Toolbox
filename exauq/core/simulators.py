7cfa01567f3e861de0497b483e7d36dbef475cdb
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-07-25 16:14:37 +0100

    Start class representing simulations log file
    This will act as an interface to the log file contain details of
    simulations that have been submitted for computation.

--- exauq/core/simulators.py
+++ exauq/core/simulators.py
@@ -1,3 +1,4 @@
+import csv
 from numbers import Real
 from os import PathLike
 from typing import Optional, Union
@@ -25,6 +26,7 @@ class Simulator(AbstractSimulator):
     previous_simulations : tuple
         (Read-only) Simulations that have been previously submitted for evaluation.
     """
+
     def __init__(self, simulations_log: Optional[Union[str, bytes, PathLike]] = None):
         self._previous_simulations = tuple()
 
@@ -39,7 +41,7 @@ class Simulator(AbstractSimulator):
     def compute(self, x: Input) -> Optional[Real]:
         """
         Compute the simulation output of an input to the simulator.
-        
+
         Parameters
         ----------
         x : Input
@@ -51,3 +53,43 @@ class Simulator(AbstractSimulator):
             ``None`` if a new input has been provided.
         """
         pass
+
+
+class SimulationsLog(object):
+    """
+    An interface to a log file containing details of simulations.
+
+    The log file contains a record of simulations that have been submitted for
+    computation. The input of each submission is recorded along with the simulator
+    output, if this has been computed. This is recorded in csv format.
+
+    Parameters
+    ----------
+    file : str, bytes or path-like, optional
+        A path to the underlying log file containing details of simulations.
+    """
+    def __init__(self, file: Optional[Union[str, bytes, PathLike]] = None):
+        self._log_file = file
+
+    def get_simulations(self):
+        """
+        Get all simulations contained in the log file.
+
+        This returns an immutable sequence of simulator inputs along with outputs. In
+        the case where the simulator output is not available for the corresponding
+        input, ``None`` is instead returned alongside the input.
+
+        Returns
+        -------
+        tuple[tuple[Input, Optional[Real]]]
+            A tuple of ``(x, y)`` pairs, where ``x`` is an `Input` and ``y`` is the
+            simulation output, or ``None`` if this hasn't yet been computed.
+        """
+        simulations = []
+        with open(self._log_file, mode="r", newline="") as log_file:
+            for row in csv.DictReader(log_file):
+                x = Input(float(row["Input"]))
+                y = float(row["Output"]) if row["Output"] else None
+                simulations.append((x, y))
+
+        return tuple(simulations)
--- tests/unit/test_simulators.py
+++ tests/unit/test_simulators.py
@@ -1,8 +1,8 @@
 import pathlib
-import unittest
+import unittest.mock
 
 from exauq.core.modelling import Input
-from exauq.core.simulators import Simulator
+from exauq.core.simulators import SimulationsLog, Simulator
 
 
 class TestSimulator(unittest.TestCase):
@@ -14,9 +14,9 @@ class TestSimulator(unittest.TestCase):
         records of previous simulations."""
 
         _ = Simulator(r"a/b/c")  # Unix
-        _ = Simulator(br"a/b/c")
+        _ = Simulator(rb"a/b/c")
         _ = Simulator(r"a\b\c")  # Windows
-        _ = Simulator(br"a\b\c")
+        _ = Simulator(rb"a\b\c")
         _ = Simulator(pathlib.Path("a/b/c"))  # Platform independent
 
     def test_previous_simulations_no_simulations_run(self):
@@ -36,6 +36,52 @@ class TestSimulator(unittest.TestCase):
         supplied."""
 
         self.assertIsNone(self.simulator.compute(Input(1)))
+
+
+class TestSimulatorLog(unittest.TestCase):
+    def setUp(self) -> None:
+        self.simulations_file = "foo.csv"
+
+    def assert_file_read(self, mock_open, file_path):
+        """Check that a mocked ``open()`` is called once in read mode on the specified
+        path."""
+
+        mock_open.assert_called_once()
+        self.assertEqual((file_path,), mock_open.call_args.args)
+        self.assertTrue(("mode", "r") in mock_open.call_args.kwargs.items())
+
+    def test_initialise_with_simulations_record_file(self):
+        """Test that a simulator log can be initialised with a handle to the log file."""
+
+        _ = SimulationsLog(r"a/b/c")  # Unix
+        _ = SimulationsLog(rb"a/b/c")
+        _ = SimulationsLog(r"a\b\c")  # Windows
+        _ = SimulationsLog(rb"a\b\c")
+        _ = SimulationsLog(pathlib.Path("a/b/c"))  # Platform independent
+
+    def test_get_simulations_no_simulations_in_file(self):
+        """Test that an empty tuple is returned if the simulations log file does not
+        contain any simulations."""
+
+        with unittest.mock.patch(
+            "builtins.open", unittest.mock.mock_open(read_data="Input,Output\n")
+        ) as mock:
+            log = SimulationsLog(self.simulations_file)
+            self.assertEqual(tuple(), log.get_simulations())
+            self.assert_file_read(mock, self.simulations_file)
+
+    def test_get_simulations_returns_simulations_from_file(self):
+        """Test that a record of all simulations (pending or otherwise) recorded
+        in the log file are returned."""
+
+        with unittest.mock.patch(
+            "builtins.open",
+            unittest.mock.mock_open(read_data="Input,Output\n1,10\n2,\n"),
+        ) as mock:
+            log = SimulationsLog(self.simulations_file)
+            expected = ((Input(1), 10), (Input(2), None))
+            self.assertEqual(expected, log.get_simulations())
+            self.assert_file_read(mock, self.simulations_file)
 
 
 if __name__ == "__main__":
