5802401e1d20cc2309809abb9c5bb542dd26fa81
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-07-25 10:24:48 +0100

    Add initialisation with a log file path
    The log file will contain a record of previously run simulations
    and inputs that have been submitted as jobs.

--- exauq/core/simulators.py
+++ exauq/core/simulators.py
@@ -1,10 +1,22 @@
 from numbers import Real
-from typing import Optional
+from os import PathLike
+from typing import Optional, Union
 
 from exauq.core.modelling import AbstractSimulator, Input
 
 
 class Simulator(AbstractSimulator):
+    """
+    Represents a simulation code that can be run on inputs.
+    
+    Parameters
+    ----------
+    simulations_log : str or bytes or os.PathLike
+        A path to the simulation log file.
+    """
+    def __init__(self, simulations_log: Union[str, bytes, PathLike]):
+        pass
+
     def compute(self, x: Input) -> Optional[Real]:
         """
         Compute the simulation output of an input to the simulator.
--- tests/unit/test_simulators.py
+++ tests/unit/test_simulators.py
@@ -1,3 +1,4 @@
+import pathlib
 import unittest
 
 from exauq.core.modelling import Input
@@ -5,11 +6,21 @@ from exauq.core.simulators import Simulator
 
 
 class TestSimulator(unittest.TestCase):
-    def test_compute_output_new_input_exist(self):
+    def test_initialise_with_simulations_record_file(self):
+        """Test that a simulator can be initialised with a handle to a file containing
+        records of previous simulations."""
+
+        _ = Simulator(r"a/b/c")  # Unix
+        _ = Simulator(br"a/b/c")
+        _ = Simulator(r"a\b\c")  # Windows
+        _ = Simulator(br"a\b\c")
+        _ = Simulator(pathlib.Path("a/b/c"))  # Platform independent
+    
+    def test_compute_output_unseen_input(self):
         """Test that None is returned as the value of a computation if a new input is
         supplied."""
 
-        simulator = Simulator()
+        simulator = Simulator(r"a/b/c")
         self.assertIsNone(simulator.compute(Input(1)))
 
 
