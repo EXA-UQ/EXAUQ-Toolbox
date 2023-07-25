c25383f9cca565f6c7278727d8e626eefc405318
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-07-25 11:03:47 +0100

    Begin support for getting previous simulations

--- exauq/core/simulators.py
+++ exauq/core/simulators.py
@@ -8,14 +8,33 @@ from exauq.core.modelling import AbstractSimulator, Input
 class Simulator(AbstractSimulator):
     """
     Represents a simulation code that can be run on inputs.
-    
+
+    Simulations that have been previously submitted for computation can be retrieved
+    using the ``previous_simulations` property. This returns a tuple of `Input`s and
+    simulation outputs; in the case where an `Input` has been submitted for evaluation
+    but no output from the simulator has been retrieved, the output is recorded as
+    ``None``.
+
     Parameters
     ----------
-    simulations_log : str or bytes or os.PathLike
-        A path to the simulation log file.
+    simulations_log : str or bytes or os.PathLike, optional
+        (Default: ``None``) A path to the simulation log file.
+
+    Attributes
+    ----------
+    previous_simulations : tuple
+        (Read-only) Simulations that have been previously submitted for evaluation.
     """
-    def __init__(self, simulations_log: Union[str, bytes, PathLike]):
-        pass
+    def __init__(self, simulations_log: Optional[Union[str, bytes, PathLike]] = None):
+        self._previous_simulations = tuple()
+
+    @property
+    def previous_simulations(self) -> tuple:
+        """
+        (Read-only) A tuple of simulations that have been previously submitted for
+        computation.
+        """
+        return self._previous_simulations
 
     def compute(self, x: Input) -> Optional[Real]:
         """
--- tests/unit/test_simulators.py
+++ tests/unit/test_simulators.py
@@ -6,6 +6,9 @@ from exauq.core.simulators import Simulator
 
 
 class TestSimulator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.simulator = Simulator()
+
     def test_initialise_with_simulations_record_file(self):
         """Test that a simulator can be initialised with a handle to a file containing
         records of previous simulations."""
@@ -15,13 +18,24 @@ class TestSimulator(unittest.TestCase):
         _ = Simulator(r"a\b\c")  # Windows
         _ = Simulator(br"a\b\c")
         _ = Simulator(pathlib.Path("a/b/c"))  # Platform independent
-    
+
+    def test_previous_simulations_no_simulations_run(self):
+        """Test that an empty tuple is returned if there are no simulations in the
+        log file."""
+
+        self.assertEqual(tuple(), self.simulator.previous_simulations)
+
+    def test_previous_simulations_immutable(self):
+        """Test that the previous_simulations property is read-only."""
+
+        with self.assertRaises(AttributeError):
+            self.simulator.previous_simulations = tuple()
+
     def test_compute_output_unseen_input(self):
         """Test that None is returned as the value of a computation if a new input is
         supplied."""
 
-        simulator = Simulator(r"a/b/c")
-        self.assertIsNone(simulator.compute(Input(1)))
+        self.assertIsNone(self.simulator.compute(Input(1)))
 
 
 if __name__ == "__main__":
