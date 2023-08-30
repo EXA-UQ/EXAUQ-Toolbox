4b1edd2a2a9e2ad622260595d759f939c5a512cb
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-08-30 14:53:07 +0100

    Clean up simulations log file

--- tests/integration/retrieving_simulations.py
+++ tests/integration/retrieving_simulations.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import time
 
@@ -5,33 +6,39 @@ from exauq.core.modelling import Input
 from exauq.core.simulators import Simulator, SimulatorDomain
 from tests.utilities.local_simulator import WORKSPACE, LocalSimulatorInterface
 
-# Initialise our interface to the local simulator, using the default workspace
-# directory.
-interface = LocalSimulatorInterface(WORKSPACE)
-
-# Initialise simulator with previously-created simulations log file
-domain = SimulatorDomain([(0, 1)] * 4)
 log_file = pathlib.Path("./simulations.csv")
-simulator = Simulator(domain, interface, log_file)
+try:
+    # Initialise our interface to the local simulator, using the default workspace
+    # directory.
+    interface = LocalSimulatorInterface(WORKSPACE)
 
-# Evaluate some simulator inputs. Notice that these evaluate immediately, but each
-# returned output is None because the computations haven't finished.
-x1 = Input(0.1, 0.1, 0.1, 0.1)
-x2 = Input(0.2, 0.2, 0.2, 0.2)
-x3 = Input(0.3, 0.3, 0.3, 0.3)
-out1 = simulator.compute(x1)
-out2 = simulator.compute(x2)
-out3 = simulator.compute(x3)
+    # Initialise simulator with previously-created simulations log file
+    domain = SimulatorDomain([(0, 1)] * 4)
+    simulator = Simulator(domain, interface, log_file)
 
-assert out1 is None
-assert out2 is None
-assert out3 is None
+    # Evaluate some simulator inputs. Notice that these evaluate immediately, but each
+    # returned output is None because the computations haven't finished.
+    x1 = Input(0.1, 0.1, 0.1, 0.1)
+    x2 = Input(0.2, 0.2, 0.2, 0.2)
+    x3 = Input(0.3, 0.3, 0.3, 0.3)
+    out1 = simulator.compute(x1)
+    out2 = simulator.compute(x2)
+    out3 = simulator.compute(x3)
 
-# Wait for the simulations to complete
-# Note: this needs to account for (1) time to run the simulations (incl. any
-# delays in picking up the jobs to run) and (2) the polling period in the job
-# manager.
-time.sleep(15)
+    assert out1 is None
+    assert out2 is None
+    assert out3 is None
 
-# Check that the previous simulations now have output values
-assert all(sim[1] is not None for sim in simulator.previous_simulations)
+    # Wait for the simulations to complete
+    # Note: this needs to account for (1) time to run the simulations (incl. any
+    # delays in picking up the jobs to run) and (2) the polling period in the job
+    # manager.
+    time.sleep(15)
+
+    # Check that the previous simulations now have output values
+    assert all(sim[1] is not None for sim in simulator.previous_simulations)
+
+finally:
+    # Clean up simulations log file
+    if log_file.exists():
+        os.remove(log_file)
