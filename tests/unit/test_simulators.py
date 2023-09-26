9ee9dc782a6f6f4aafdc8e2a79a202ba053ab2d4
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2023-09-26 12:12:47 +0100

    Move unit tests around
    So that they better match up with the order in which they appear in
    the main source file.

--- tests/unit/test_simulators.py
+++ tests/unit/test_simulators.py
@@ -419,49 +419,6 @@ class TestSimulationsLog(unittest.TestCase):
         log.add_new_record(x2)
         self.assertEqual(((x1, None), (x2, None)), log.get_simulations())
 
-    def test_get_unsubmitted_inputs_no_inputs(self):
-        """Test that an empty tuple is returned if no inputs have been submitted to the
-        log file."""
-
-        log = SimulationsLog(self.simulations_file, num_inputs=1)
-        self.assertEqual(tuple(), log.get_unsubmitted_inputs())
-
-    def test_get_unsubmitted_inputs_unsubmitted_input(self):
-        """Test that, when an input is submitted with no job ID, it features as an
-        unsubmitted input."""
-
-        x = Input(1)
-        log = SimulationsLog(self.simulations_file, num_inputs=len(x))
-        log.add_new_record(x)
-        self.assertEqual((x,), log.get_unsubmitted_inputs())
-
-    def test_get_unsubmitted_inputs_submitted_input(self):
-        """Test that, when an input is submitted with a job ID, it does not feature as
-        an unsubmitted input."""
-
-        x = Input(1)
-        job_id = "0"
-        log = SimulationsLog(self.simulations_file, num_inputs=len(x))
-        log.add_new_record(x, job_id)
-        self.assertTrue(x not in log.get_unsubmitted_inputs())
-
-    def test_get_unsubmitted_inputs_submitted_unsubmitted_mix(self):
-        """Test that, when several inputs are submitted, only those that weren't
-        submitted with a job ID are returned as unsubmitted inputs."""
-
-        x1 = Input(1)
-        x2 = Input(2)
-        x3 = Input(3)
-        x4 = Input(4)
-        log = SimulationsLog(self.simulations_file, num_inputs=len(x1))
-        log.add_new_record(x1, job_id="0")
-        log.add_new_record(x2)
-        log.add_new_record(
-            x3,
-        )
-        log.add_new_record(x4, job_id="1")
-        self.assertEqual((x2, x3), log.get_unsubmitted_inputs())
-
     def test_insert_result_output_added(self):
         """Test that a simulator output is added alongside a simulator input and
         can be retrieved from the previous simulations."""
@@ -535,6 +492,49 @@ class TestSimulationsLog(unittest.TestCase):
         }
         log = self.make_simulations_log(data)
         self.assertEqual(("2", "3"), log.get_pending_jobs())
+
+    def test_get_unsubmitted_inputs_no_inputs(self):
+        """Test that an empty tuple is returned if no inputs have been submitted to the
+        log file."""
+
+        log = SimulationsLog(self.simulations_file, num_inputs=1)
+        self.assertEqual(tuple(), log.get_unsubmitted_inputs())
+
+    def test_get_unsubmitted_inputs_unsubmitted_input(self):
+        """Test that, when an input is submitted with no job ID, it features as an
+        unsubmitted input."""
+
+        x = Input(1)
+        log = SimulationsLog(self.simulations_file, num_inputs=len(x))
+        log.add_new_record(x)
+        self.assertEqual((x,), log.get_unsubmitted_inputs())
+
+    def test_get_unsubmitted_inputs_submitted_input(self):
+        """Test that, when an input is submitted with a job ID, it does not feature as
+        an unsubmitted input."""
+
+        x = Input(1)
+        job_id = "0"
+        log = SimulationsLog(self.simulations_file, num_inputs=len(x))
+        log.add_new_record(x, job_id)
+        self.assertTrue(x not in log.get_unsubmitted_inputs())
+
+    def test_get_unsubmitted_inputs_submitted_unsubmitted_mix(self):
+        """Test that, when several inputs are submitted, only those that weren't
+        submitted with a job ID are returned as unsubmitted inputs."""
+
+        x1 = Input(1)
+        x2 = Input(2)
+        x3 = Input(3)
+        x4 = Input(4)
+        log = SimulationsLog(self.simulations_file, num_inputs=len(x1))
+        log.add_new_record(x1, job_id="0")
+        log.add_new_record(x2)
+        log.add_new_record(
+            x3,
+        )
+        log.add_new_record(x4, job_id="1")
+        self.assertEqual((x2, x3), log.get_unsubmitted_inputs())
 
 
 if __name__ == "__main__":
