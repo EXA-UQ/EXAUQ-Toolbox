33ca5ba2dc5771116729caf32734dfd7974386c3
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-01-24 18:04:46 +0000

    Add some argument validation to maximise

--- exauq/utilities/optimisation.py
+++ exauq/utilities/optimisation.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+import numpy as np
 import scipy.optimize
 from numpy.typing import NDArray
 
@@ -8,6 +9,19 @@ from exauq.core.numerics import FLOAT_TOLERANCE
 
 
 def maximise(func: Callable[[NDArray], float], domain: SimulatorDomain) -> Input:
+    if not isinstance(domain, SimulatorDomain):
+        raise TypeError(
+            f"Expected 'domain' to be of type SimulatorDomain, but received {type(domain)} instead."
+        )
+
+    try:
+        _ = func(np.array(domain.scale([0.5] * domain.dim)))
+    except Exception:
+        raise ValueError(
+            "Expected 'func' to be a callable that takes a 1-dim Numpy array as argument "
+            "and returns a float."
+        )
+
     result = scipy.optimize.differential_evolution(
         lambda x: -func(x),
         bounds=domain._bounds,
--- tests/unit/test_optimisation.py
+++ tests/unit/test_optimisation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from exauq.core.modelling import Input, SimulatorDomain
 from exauq.utilities.optimisation import maximise
-from tests.utilities.utilities import ExauqTestCase
+from tests.utilities.utilities import ExauqTestCase, exact
 
 
 class TestMaximise(ExauqTestCase):
@@ -36,6 +36,32 @@ class TestMaximise(ExauqTestCase):
         x = maximise(f, domain)
         argmax = Input(5 * np.pi / 4)
         self.assertEqualWithinTolerance(argmax, x, rel_tol=1e-5)
+
+    def test_arg_errors(self):
+        """A ValueError is raised if the provided function does not accept Numpy arrays as
+        args. A TypeError is raised the supplied domain is not of type SimulatorDomain."""
+        domain = SimulatorDomain([(0, 1)])
+
+        def f(x: dict) -> float:
+            return x["a"]
+
+        with self.assertRaisesRegex(
+            ValueError,
+            exact(
+                "Expected 'func' to be a callable that takes a 1-dim Numpy array as argument "
+                "and returns a float."
+            ),
+        ):
+            _ = maximise(f, domain)
+
+        arg = "a"
+        with self.assertRaisesRegex(
+            TypeError,
+            exact(
+                f"Expected 'domain' to be of type SimulatorDomain, but received {type(arg)} instead."
+            ),
+        ):
+            _ = maximise(np.sum, domain=arg)
 
 
 if __name__ == "__main__":
