3264317f87a9db8a09a9cc4ee63bdb6289587849
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-02-04 19:24:57 +0000

    Add docstring to read_from_csv

--- exauq/core/modelling.py
+++ exauq/core/modelling.py
@@ -347,6 +347,47 @@ class TrainingDatum(object):
     def read_from_csv(
         cls, path: Path, output_col: int = -1, header: bool = False
     ) -> tuple[TrainingDatum, ...]:
+        """Read simulator inputs and outputs from a csv file.
+
+        The data from the csv file is parsed into a sequence of TrainingDatum objects,
+        with one datum per (non-empty) row in the csv file. By default, the last column is
+        assumed to contain the simulator outputs, though an alternative can be specified
+        via the `output_col` keyword argument. The remaining columns are assumed to define
+        the coordinates of simulator inputs, in the same order in which they appear in the
+        csv file. If the csv file contains a header row then this should be specified so
+        that it can be skipped when reading.
+
+        While it is expected that the data in the csv will be rectangular (i.e. each row
+        contains the same number of columns), csv files with varying numbers of columns in
+        each row will be parsed, so long as the `output_col` is valid for each row. (Users
+        should take care in this case, as the various ``TrainingDatum`` constructed will
+        have simulator inputs of varying dimension.)
+
+        Parameters
+        ----------
+        path : str or os.PathLike
+            The path to a csv file.
+        output_col : int, optional
+            (Default: -1) The (0-based) index of the column that defines the simulator
+            outputs. Negative values count backwards from the end of the list of columns
+            (the default Python behaviour). The default value corresponds to the last
+            column in each row.
+        header : bool, optional
+            (Default: False) Whether the csv contains a header row that should be skipped.
+
+        Returns
+        -------
+        tuple[TrainingDatum, ...]
+            The training data read from the csv file.
+
+        Raises
+        ------
+        AssertionError
+            If the training data contains values that cannot be parsed as finite,
+            non-missing floats.
+        ValueError
+            If 'output_col' does not define a valid column index for all rows.
+        """
         training_data = []
         with open(path, mode="r", newline="") as csvfile:
             reader = enumerate(csv.reader(csvfile))
