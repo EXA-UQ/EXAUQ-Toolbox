1ac23a8fd5c957a721de2820a0ce4902fa2ec4ae
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-02-09 14:47:39 +0000

    Add job id validation
    Ensure it defines a string consisting only of digits.

--- exauq/core/jobs.py
+++ exauq/core/jobs.py
@@ -1,6 +1,19 @@
+import re
 from typing import Union
 
 
 class JobId:
     def __init__(self, job_id: Union[str, int]):
-        self._job_id = job_id
+        self._job_id = self._parse(job_id)
+
+    @staticmethod
+    def _parse(job_id) -> str:
+        job_id_str = str(job_id)
+        if re.fullmatch("[0-9]+", job_id_str):
+            return job_id_str
+        else:
+            raise ValueError(
+                "Expected 'job_id' to define a string consisting only of digits, "
+                f"but received '{str(job_id)}' instead."
+            )
+
--- tests/unit/test_jobs.py
+++ tests/unit/test_jobs.py
@@ -1,21 +1,37 @@
 import unittest
 
 from exauq.core.jobs import JobId
-from tests.utilities.utilities import ExauqTestCase
+from tests.utilities.utilities import ExauqTestCase, exact
 
 
 class TestJobId(ExauqTestCase):
     def test_create_from_integer_string_like(self):
-        """A job ID can be created from an object whose string representation is a
-        positive integer."""
+        """A job ID can be created from a non-negative integer or a string consisting only
+        of digits."""
 
-        for job_id in ["1", 99]:
+        for job_id in ["1", 99, "00001"]:
             try:
                 _ = JobId(job_id)
             except Exception:
                 self.fail(
-                    f"Should have been able to construct JobId with job_id = {job_id}"
+                    f"Should have been able to construct JobId with job_id = {job_id}."
                 )
+
+    def test_non_integer_string_like_error(self):
+        """A ValueError is raised if the supplied job ID whose string representation
+        contains characters other than digits."""
+
+        for job_id in ["", "rm -rf ~", "!", -1, "0.1", "1e10", 2j, [1]]:
+            with self.subTest(job_id=job_id):
+                with self.assertRaisesRegex(
+                    ValueError,
+                    exact(
+                        "Expected 'job_id' to define a string consisting only of digits, "
+                        f"but received '{str(job_id)}' instead."
+                    ),
+                ):
+                    _ = JobId(job_id)
+
 
 
 if __name__ == "__main__":
