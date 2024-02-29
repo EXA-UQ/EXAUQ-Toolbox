dc0585cb6fe6665d9da65151c0dd9579d0cc1280
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2024-02-29 15:07:06 +0000

    Changed `_input_keys` type to match db interface

--- exauq/sim_management/simulators.py
+++ exauq/sim_management/simulators.py
@@ -163,12 +163,12 @@ class SimulationsLog(object):
         self._job_id_key = "Job_ID"
         self._output_key = "Output"
         self._job_status_key = "Job_Status"
-        self._input_keys = tuple(f"Input_{i}" for i in range(1, self._input_dim + 1))
-        self._log_file_header = self._input_keys + (
+        self._input_keys = list(f"Input_{i}" for i in range(1, self._input_dim + 1))
+        self._log_file_header = self._input_keys + [
             self._output_key,
             self._job_id_key,
             self._job_status_key,
-        )
+        ]
         self._log_file = self._initialise_log_file(file)
         self._simulations_db = self._make_db(self._log_file, self._log_file_header)
 
