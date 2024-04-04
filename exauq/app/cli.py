74830c87e2022334c8dfc5b125e1e8f4664f22b1
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-04-04 14:12:19 +0100

    Add placeholders for main commands

--- exauq/app/cli.py
+++ exauq/app/cli.py
@@ -3,16 +3,46 @@ import sys
 import cmd2
 
 
-class App(cmd2.Cmd):
+class App:
+    def __init__(self):
+        pass
+
+    def submit(self) -> str:
+        return "** Submitted something. **"
+
+    def status(self) -> str:
+        return "** Shows status of some jobs. **"
+
+    def result(self) -> str:
+        return "** Gets the result of jobs. **"
+
+
+class Cmd2AppWrapper(cmd2.Cmd):
     """The exauq command line application for managing jobs."""
 
     def __init__(self):
         super().__init__()
+        self.app = App()
         self.prompt = "(exauq)> "
+
+    def do_submit(self, args) -> None:
+        """Submit a job to the simulator."""
+
+        self.poutput(self.app.submit())
+
+    def do_status(self, args) -> None:
+        """Get the status of simulation jobs."""
+
+        self.poutput(self.app.status())
+
+    def do_result(self, args) -> None:
+        """Retrieve the result of simulation jobs"""
+
+        self.poutput(self.app.result())
 
 
 def main():
-    app = App()
+    app = Cmd2AppWrapper()
     sys.exit(app.cmdloop())
 
 
