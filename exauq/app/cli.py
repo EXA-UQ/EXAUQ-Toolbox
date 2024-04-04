a97f820e1a917106f48ff3eb581528c7e418a06b
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-04-04 13:41:58 +0100

    Add skeleton interactive CLA using cmd2

--- exauq/app/cli.py
+++ exauq/app/cli.py
@@ -1,5 +1,19 @@
+import sys
+
+import cmd2
+
+
+class App(cmd2.Cmd):
+    """The exauq command line application for managing jobs."""
+
+    def __init__(self):
+        super().__init__()
+        self.prompt = "(exauq)> "
+
+
 def main():
-    print("Hello, World!")
+    app = App()
+    sys.exit(app.cmdloop())
 
 
 if __name__ == "__main__":
