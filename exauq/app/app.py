cca8874e9cda41ac2c0226f4ab0269c575c28ebf
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2024-04-05 12:01:25 +0100

    Added `App.__init__` functionality

--- exauq/app/app.py
+++ exauq/app/app.py
@@ -1,10 +1,30 @@
 from collections.abc import Sequence
 from numbers import Real
 
+from exauq.core.modelling import Input
+from exauq.sim_management.hardware import HardwareInterface
+from exauq.sim_management.jobs import Job
+from exauq.sim_management.simulators import JobManager, SimulationsLog
+from exauq.sim_management.types import FilePath
+
 
 class App:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        interface: HardwareInterface,
+        input_dim: int,
+        simulations_log_file: FilePath = "simulations.csv",
+    ):
+        self._sim_log_path = simulations_log_file
+        self._input_dim = input_dim
+        self._interface = interface
+        self._sim_log = SimulationsLog(self._sim_log_path, self._input_dim)
+        self._job_manager = JobManager(
+            simulations_log=self._sim_log,
+            interface=self._interface,
+            polling_interval=10,
+            wait_for_pending=False,
+        )
 
     # TODO: Return type just for illustration, not necessarily final API
     def submit(self, inputs: Sequence[Sequence[Real]]) -> dict[str, tuple[float, ...]]:
