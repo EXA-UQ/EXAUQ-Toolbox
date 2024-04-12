667b85ae9ef888395c8bacab02b006c442ac7ac0
Thomas Hawes <105276609+thawes-rse@users.noreply.github.com>
2024-04-12 16:12:36 +0100

    Add default hardware interface factory class
    Builds an object of a given hardware interface class by getting
    parameter values from user input. Does no parsing of these values.

--- exauq/app/startup.py
+++ exauq/app/startup.py
@@ -1,21 +1,69 @@
+import inspect
 import json
-from typing import Any
+from typing import Any, Optional
 
-from exauq.sim_management.hardware import UnixServerScriptInterface
+from exauq.sim_management.hardware import HardwareInterface, UnixServerScriptInterface
 from exauq.sim_management.types import FilePath
 
 
-class InteractiveUnixServerScriptInterfaceFactory:
-    def __init__(self):
+class HardwareInterfaceFactory:
+    def __init__(self, hardware_cls: type):
+        self._hardware_cls = hardware_cls
         self._hardware_parameters = None
 
     @property
-    def hardware_parameters(self) -> dict[str, Any]:
+    def hardware_parameters(self) -> Optional[dict[str, Any]]:
         return self._hardware_parameters
 
     @property
     def hardware_type(self) -> str:
-        return UnixServerScriptInterface.__name__
+        return self._hardware_cls.__name__
+
+    def make_hardware_interactively(self) -> HardwareInterface:
+        cls_init_params = [
+            param
+            for param in inspect.signature(
+                self._hardware_cls.__init__
+            ).parameters.values()
+            if not param.name == "self"
+        ]
+        params = dict()
+        for param in cls_init_params:
+            if param.default is inspect.Parameter.empty:
+                default_value = None
+                value_str = input(f"{self.hardware_type} {param.name}: ").strip()
+            else:
+                default_value = param.default
+                value_str = input(
+                    f"{self.hardware_type} {param.name} (default: {default_value}): "
+                ).strip()
+
+            value = value_str if value_str else default_value
+            params[param.name] = value
+
+        self._hardware_parameters = params
+        return self._hardware_cls(**params)
+
+    def serialise_hardware_parameters(self, params_file: FilePath) -> None:
+        with open(params_file, mode="w") as f:
+            json.dump(self.hardware_parameters, f, indent=4)
+
+    def load_hardware(self, params_file: FilePath) -> HardwareInterface:
+        with open(params_file, mode="r") as f:
+            params = json.load(f)
+
+        self._hardware_parameters = params
+        return self._hardware_cls(**params)
+
+
+class InteractiveUnixServerScriptInterfaceFactory(HardwareInterfaceFactory):
+    def __init__(self):
+        super().__init__(UnixServerScriptInterface)
+        self._hardware_parameters = None
+
+    @property
+    def hardware_parameters(self) -> Optional[dict[str, Any]]:
+        return self._hardware_parameters
 
     def make_hardware_interactively(self) -> UnixServerScriptInterface:
         host = input("Host server address: ")
@@ -32,13 +80,3 @@ class InteractiveUnixServerScriptInterfaceFactory:
         params["workspace_dir"] = hardware.workspace_dir
         self._hardware_parameters = params
         return hardware
-
-    def serialise_hardware_parameters(self, params_file: FilePath) -> None:
-        with open(params_file, mode="w") as f:
-            json.dump(self.hardware_parameters, f, indent=4)
-
-    def load_hardware(self, params_file: FilePath) -> UnixServerScriptInterface:
-        with open(params_file, mode="r") as f:
-            params = json.load(f)
-
-        return UnixServerScriptInterface(**params)
