7ddf5d86c6263102b143287c831c0be1f48fc96c
Harry White <181727566+HarryWhiteRSE@users.noreply.github.com>
2024-09-20 14:53:52 +0100

    raise errors for incorrect argument types

--- exauq/utilities/oneshot-designer.py
+++ exauq/utilities/oneshot-designer.py
@@ -9,6 +9,21 @@ def oneshot(
     domain: SimulatorDomain, design_num: int, seed: Optional[int] = None   
 ) -> tuple[Input, float]:
     
+    if not isinstance(domain, SimulatorDomain):
+        raise TypeError(
+            f'Expected domain to be of type SimulatorDomain, but received {type(domain)}'
+        )
+    
+    if not isinstance(design_num, int):
+        raise TypeError(
+            f'Expected design_num to be of type int, but received {type(design_num)}'
+        )
+
+    if seed is not None and not isinstance(seed, int):
+        raise TypeError(
+            f'Expected seed to be None or of type int, but received {type(seed)}'
+        )
+    
     # Use the dimension of the domain in defining the Latin hypercube sampler.
     # Seed used to make the sampling repeatable.
     sampler = LatinHypercube(domain.dim, seed)
