cfbde2e4ec6a0f69176a8cdb3e096eb29b0184a9
Harry White <181727566+HarryWhiteRSE@users.noreply.github.com>
2024-10-15 11:01:21 +0100

    Preserve metadata using functools.wraps in suppress_print decorator

--- exauq/utilities/decorators.py
+++ exauq/utilities/decorators.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from functools import wraps
 import contextlib
 
 
@@ -6,6 +7,7 @@ def suppress_print(func):
     """This decorator is implemented to redirect the printing of unnecessary print statements from certain libraries
     (e.g the mogp-emulator) away from stdio to focus on only relevent information."""
 
+    @wraps(func)
     def wrapper(*args, **kwargs):
         with contextlib.redirect_stdout(StringIO()):
             return func(*args, **kwargs)
