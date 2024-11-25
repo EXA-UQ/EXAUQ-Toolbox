48a1d9c1251c2d4a17cc80f5c8398c111a277b51
Harry White <181727566+HarryWhiteRSE@users.noreply.github.com>
2024-11-25 11:40:45 +0000

    Fix key error in _remove_multi_level_repeated_input

--- exauq/core/designers.py
+++ exauq/core/designers.py
@@ -1472,9 +1472,13 @@ def _remove_multi_level_repeated_input(
 
     # Todo: write docstrings and tests.
 
-    for lvl in range(min(data.levels), level):
-        data[lvl] = [
-            dat for dat in data[lvl] if not equal_within_tolerance(dat.input, datum.input)
-        ]
-
+    for lvl in data.levels:
+        if lvl < level:
+            data[lvl] = [
+                dat
+                for dat in data[lvl]
+                if not equal_within_tolerance(dat.input, datum.input)
+            ]
+        else:
+            return data
     return data
