8c2a78b83a2bcbad437783f180a26d7a1b39df44
Matt Johns <96123593+mbjohns@users.noreply.github.com>
2025-02-13 13:46:28 +0000

    `exauq.core`- Reformat package docstring

--- exauq/core/__init__.py
+++ exauq/core/__init__.py
@@ -1,9 +1,5 @@
 """
-exauq.core
-===========
-
--------------------------------------------------------------------------------------------
-The exauq core package contains everything required for the creation and training 
+The exauq core package contains everything required for the creation and training
 of both single level and multi-level deterministic Gaussian process (GP) emulators 
 and leave-one-out (LOO) sampling methods on customisable designs. The GP emulation
 is built upon the `mogp_emulator` while implementing the ability to bound hyperparameters. 
@@ -11,30 +7,28 @@ Spatial domains can be created and filled with effective sampled points alongsid
 repulsion points across the boundary. 
 
 Tutorials on how to create a basic experimental design can be found within the 
-Experimental Design tutorials. 
+[Experimental Design tutorials](../../designers/tutorials/).
 
--------------------------------------------------------------------------------------------
-Modules
-=========
 
-[`designers`][exauq.core.designers]:
-    Create the experimental domain using either a simple oneshot Latin hypercube or through
-    the LOO sampling methods for both single and multi-level GPs.
+ Modules
+------------------------------------------------------------------------------------------------
+[`designers`][exauq.core.designers]
+Create the experimental domain using either a simple oneshot Latin hypercube or through the
+LOO sampling methods for both single and multi-level GPs.
 
-[`emulators`][exauq.core.emulators]:
-    Provides the emulators to modify and train the GP and control hyperparameters.
+[`emulators`][exauq.core.emulators]
+Provides the emulators to modify and train the GP and control hyperparameters.
     
-[`modelling`][exauq.core.modelling]:
-    Contains the objects required for utilising and training GP emulators alongside the
-    LOO sampling methods for both single and multi-level GPs.
+[`modelling`][exauq.core.modelling]
+Contains the objects required for utilising and training GP emulators alongside the LOO
+sampling methods for both single and multi-level GPs.
 
-[`numerics`][exauq.core.numerics]:
-    Numerical tolerance checks.
+[`numerics`][exauq.core.numerics]
+Numerical tolerance checks.
 
 
 References 
--------------------------------------------------------------------------------------------
-
+------------------------------------------------------------------------------------------------------
 `mogp_emulator`:  <https://github.com/alan-turing-institute/mogp-emulator>
 
 `LOO Sampling`: 
